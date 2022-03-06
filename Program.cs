25b25163b7df80d6657b20a1848af16b5153f1e4
Sasha-Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-03-06 20:32:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace MorseCode
+{
+    class Program
+    {
+        // Morse code translation
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string name0 = Console.ReadLine().ToUpper();
+            string nameMorse0 = "";
+
+            foreach (char letter0 in name0)
+            {
+                nameMorse0 += Convert.ToString(tableMorse[letter0]);
+            }
+            Console.WriteLine("Your name in Morse: {0}", nameMorse0);
+            Console.ReadLine();
+        }
+
+        static Hashtable tableMorse = new Hashtable()
+    { {'A',".-"   },
+      {'B',"-..." },
+      {'C',"-.-." },
+      {'D',"-.."  },
+      {'E', "."   },
+      {'F',"..-." },
+      {'G',"--."  },
+      {'H',"...." },
+      {'I',".."   },
+      {'J',".---" },
+      {'K',"-.-"  },
+      {'L',".-.." },
+      {'M',"--"   },
+      {'N',"-."   },
+      {'O',"---"  },
+      {'P',".--." },
+      {'Q',"--.-" },
+      {'R', ".-." },
+      {'S', "..." },
+      {'T',"-"    },
+      {'U',"..-"  },
+      {'V',"...-" },
+      {'W',".--"  },
+      {'X',"-..-" },
+      {'Y',"-.--" },
+      {'Z',"--.." },
+      {'0',"-----"},
+      {'1',".----"},
+      {'2',"..---"},
+      {'3',"...--"},
+      {'4',"....-"},
+      {'5',"....."},
+      {'6',"-...."},
+      {'7',"--..."},
+      {'8',"---.."},
+      {'9',"----."}
+    };
+    }
+}
